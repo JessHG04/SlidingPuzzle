bd6108c8b47837d0714d3d66d7723bcf15b6445d
Jessica Hernández Gómez <jessicahg04@gmail.com>
2021-08-10 10:49:41 +0200

    Bucles infinitos

--- Sliding Puzzle/Assets/Scripts/Board.cs
+++ Sliding Puzzle/Assets/Scripts/Board.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,89 +7,163 @@ public class Board : MonoBehaviour {
     private int _columns;
     private int _minMoves = 0;
     private int[][] _dir = new int[][] { new int[] { 1, 0 }, new int[] { -1, 0 }, new int[] { 0, 1 }, new int[] { 0, -1 } };
-    private List<List<Tile>> _boardList = new List<List<Tile>>();
+    private List<int> _boardList = new List<int>();
 
     private void Awake() {
         _instance = this;
     }
 
     public void InitList(List<Tile> list) {
-        for(int i = 0; i < _rows; i++) {
-            _boardList.Add(new List<Tile>());
-            for(int j = 0; j < _columns; j++) {
-                _boardList[i].Add(list[i * _columns + j]);
+        for(int x = 0; x < list.Count; x++) {
+            if(list[x] != null){
+                _boardList.Add(list[x].id);
+            }else {
+                _boardList.Add(0);
             }
         }
 
         CalculateMinMoves();
         Debug.Log("Min moves: " + _minMoves);
         
-        // See List content
-        //for(int i = 0; i < _rows; i++) {
-        //    Debug.Log("Fila: " + i);
-        //    for(int j = 0; j < _columns; j++) {
-        //        if(_boardList[i][j] != null) {
-        //            Debug.Log(_boardList[i][j].id);
-        //        }else{
-        //            Debug.Log(0);
-        //        }
-        //    }
-        //}
+       
     }
 
-    public void CalculateMinMoves(){
+    private void SeeList(List<Tile> board) {
+        // See List content
+        for(int i = 0; i < board.Count; i++) {
+            if(board[i] != null) {
+                Debug.Log(board[i].id);
+            }else{
+                Debug.Log(0);
+            }
+        }
+    }
+
+    private void SeeBoardlist() {
+        // See Board List
+        Debug.Log("Board list");
+        for(int i = 0; i < _boardList.Count; i++) {
+            Debug.Log(_boardList[i]);
+        }
+    }
+
+    /*public void CalculateMinMoves(){
         Queue <List<List<Tile>>> q = new Queue<List<List<Tile>>>();
         q.Enqueue(_boardList);
         SortedSet <List<List<Tile>>> visited = new SortedSet<List<List<Tile>>>();
         visited.Add(_boardList);
 
         for(int lvl = 1; q.Count != 0; lvl++) {
-            List<List<Tile>> node = q.Dequeue();
-            int x = -1;
-            int y = -1;
-            for (int i = 0; i < _boardList.Count; i++) {
-                for (int j = 0; j < _boardList[i].Count; j++) {
-                    if(node[i][j] == null){
-                        x = i;
-                        y = j;
-                        break;
+            int sz = q.Count;
+            Debug.Log("Sz: " + sz);
+            while(sz-- != 0){
+                List<List<Tile>> node = q.Dequeue();
+                int x = -1;
+                int y = -1;
+                for (int i = 0; i < _boardList.Count; i++) {
+                    for (int j = 0; j < _boardList[i].Count; j++) {
+                        if(node[i][j] == null){
+                            x = i;
+                            y = j;
+                            break;
+                        }
                     }
                 }
-            }
 
-            //Hasta aqui, nice
-
-            for(int k = 0; k < _rows; k++) {
-                int nx = x + _dir[k][0];
-                int ny = y + _dir[k][1];
-                //Debug.Log(nx + " " + ny);
-                if(nx < 0 || ny < 0 || nx >= _rows || ny >= _columns){
+                for(int k = 0; k < _rows; k++) {
+                    int nx = x + _dir[k][0];
+                    int ny = y + _dir[k][1];
+                    //Debug.Log(nx + " " + ny);
+                    if(nx < 0 || ny < 0 || nx >= _rows || ny >= _columns) continue;
+                    //Swap contents
+                    Tile temp = node[x][y];
+                    node[x][y] = node[nx][ny];
+                    node[nx][ny] = temp;
+                    //Add to visited
+                    if(visited.Contains(node)){
+                        //Swap back
+                        node[x][y] = node[nx][ny];
+                        node[nx][ny] = temp;
+                        continue;
+                    }
                     Debug.Log("a");
-                    continue;
-                }
-                //Swap contents
-                Tile temp = node[x][y];
-                node[x][y] = node[nx][ny];
-                node[nx][ny] = temp;
-                //Add to visited
-                if(visited.Contains(node)){
+                    visited.Add(node);
+                    if(GameManager.GetInstance().CheckTiles()){
+                        _minMoves = lvl;
+                        break;
+                    }
+                    //Add to queue
+                    q.Enqueue(node);
+                    
+                    Debug.Log(q.Count);
                     //Swap back
                     node[x][y] = node[nx][ny];
                     node[nx][ny] = temp;
-                    continue;
                 }
-                visited.Add(node);
-                if(GameManager.GetInstance().CheckTiles()){
-                    _minMoves = lvl;
-                    break;
+            }
+            
+        }
+    }*/
+
+    public int CalculateMinMoves() {
+        int movements = 0;
+        if(GameManager.GetInstance().CheckTiles()) return movements;
+        List<List<Tile>> q = _boardList;
+        //Debug.Log("Q");
+        //SeeList(q);
+        //List<List<Tile>> visited = new List<List<Tile>>();
+        List<Tile> visited = new List<Tile>();
+        //Debug.Log("visited");
+        //SeeList(visited);
+        Debug.Log(q.Count + " " + q[0].Count);
+        Debug.Log("Elements: " + q.Count * q[0].Count);
+        int sz = _rows * _columns;
+        for(int lvl = 1; sz != 0; lvl++) {
+            sz = (_rows * _columns) - ((lvl - 1) * _rows);
+            Debug.Log("Sz: " + sz);
+            while(sz != 0) {
+                //Dequeue
+                int x = -1;
+                int y = -1;
+                for (int i = 0; i < q.Count; i++) {
+                    for (int j = 0; j < q[i].Count; j++) {
+                        if(q[i][j] == null) {
+                            x = i;
+                            y = j;
+                            break;
+                        }
+                    }
                 }
-                //Add to queue
-                q.Enqueue(node);
-                //Swap back
-                node[x][y] = node[nx][ny];
-                node[nx][ny] = temp;
+                Debug.Log("x: " + x + " y: " + y);
+                
+                for(int k = 0; k < _rows; k++) {
+                    int nx = x + _dir[k][0];
+                    int ny = y + _dir[k][1];
+                    //Debug.Log(nx + " " + ny);
+                    if(nx < 0 || ny < 0 || nx >= _rows || ny >= _columns) continue;
+                    //Swap contents
+                    Tile temp = q[x][y];
+                    q[x][y] = q[nx][ny];
+                    q[nx][ny] = temp;
+                    //Add to visited
+                    if(visited.Contains(q[x][y])){
+                        //Swap back
+                        q[x][y] = q[nx][ny];
+                        q[nx][ny] = temp;
+                        continue;
+                    }
+                    visited.Add(q[x][y]);
+                    Debug.Log("Visited: " + visited.Count);
+                    SeeList(visited);
+
+                    if(GameManager.GetInstance().CheckTiles()) return lvl;
+
+                }
             }
         }
+
+        return movements;
+
     }
 
     public void SetRows(int rows) => _rows = rows;
